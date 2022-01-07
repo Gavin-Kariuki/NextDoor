a13327211fb46ac7bd0899e9a2f4b0891c2f20e1
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-01-07 15:43:17 +0300

    add urls for the app

---
+++ door/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.index, name = 'home_page'),
+    path("add_hood/", views.create_mtaa, name = "add_hood"),
+    path("new_post/", views.new_post, name = "new_post"),
+    path("posts/", views.post, name = "post"),
+    path("profile/", views.profile, name = "profile"),
+    path("business/", views.new_business, name = "business"),
+    path("logout/", views.logout, name = "logout"),
+
+]
